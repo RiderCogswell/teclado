cef89f11a2452bf829ec4c99fdfacb5028926aed
Ridercogswell <ridercogswell@gmail.com>
2022-08-24 00:05:35 -0700

    init

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+def create_app(test_config=None):
+  # setup config
+  app = Flask(__name__, static_url_path='/')
+  app.url_map.strict_slashes = False
+  app.config.from_mapping(
+    SECRET_KEY='super_secret_key'
+  )
+
+  @app.route('/hello')
+  def hello():
+    return 'hello world'
+
+  return app
+
