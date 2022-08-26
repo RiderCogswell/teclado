ebc7c7c8ebb5a489f45aadb2a7420f37ae6e2750
Ridercogswell <ridercogswell@gmail.com>
2022-08-25 23:10:46 -0700

    models

---
+++ app/models/User.py
@@ -0,0 +1,10 @@
+from enum import unique
+from app.db import declarative_base
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+  __tablename__ = 'users'
+  id = Column(Integer, primary_key=True)
+  username = Column(String(50), nullable=False)
+  email = Column(String(50), nullable=False, unique=True)
+  password = Column(String(100), nullable=False)
