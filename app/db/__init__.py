ce9aad377822f2fe5da7f2a12f1387778567c14e
Ridercogswell <ridercogswell@gmail.com>
2022-08-25 22:41:35 -0700

    sql setup

---
+++ app/db/__init__.py
@@ -0,0 +1,11 @@
+from os import getenv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
