0c868502c1f9ec3969e8c3f97652eaa66de71ee9
Ridercogswell <ridercogswell@gmail.com>
2022-08-29 20:22:36 -0700

    comment model

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User
+from app.models import User, Post
 from app.db import Session, Base, engine
 
 # drop and rebuild tables
@@ -17,5 +17,16 @@ db.add_all([ # db.add_all() does not actually insert the queries!
 ])
 
 db.commit() # in order to run the above statements, must run db.commit()
+
+# insert posts
+db.add_all([
+  Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
+  Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
+  Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
+  Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
+  Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
+])
+
+db.commit()
 
 db.close() # close session
