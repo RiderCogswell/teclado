106df57f0c8538d5da660e9b4e29da148176d1d0
Ridercogswell <ridercogswell@gmail.com>
2022-08-29 23:05:10 -0700

    implement voting

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
 class Post(Base):
   __tablename__ = 'posts'
@@ -11,6 +12,10 @@ class Post(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+  vote_count = column_property(
+    select([func.count(Vote.id)]).where(Vote.post_id == id)
+  )
 
   user = relationship('User')
   comments = relationship('Comment', cascade='all,delete')
+  votes = relationship('Vote', cascade='all,delete')
