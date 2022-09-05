6706668e2bfd4a9e24e993b9dd23d37533c71539
Ridercogswell <ridercogswell@gmail.com>
2022-09-04 23:50:53 -0700

    format url

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -1,5 +1,8 @@
 def format_date(date):
   return date.strftime('%m/%d/%y')
 
+def format_url(url):
+  return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0] # cleans
+
 from datetime import datetime
 print(format_date(datetime.now()))
