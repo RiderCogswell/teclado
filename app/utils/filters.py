23ae6e1fd8637e2b1916cb1386070cf431ed9044
Ridercogswell <ridercogswell@gmail.com>
2022-09-04 23:53:24 -0700

    format plural

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -4,5 +4,17 @@ def format_date(date):
 def format_url(url):
   return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0] # cleans
 
+def format_plural(amount, word):
+  if amount != 1:
+    return word + 's'
+  
+  return word
+
 from datetime import datetime
 print(format_date(datetime.now()))
+
+print(format_url('http://google.com/test/'))
+print(format_url('https://www.google.com?q=test'))
+
+print(format_plural(2, 'cat'))
+print(format_plural(1, 'dog'))
