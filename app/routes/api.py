250d3a36d781ef7df294d71f52f30832773e8395
Ridercogswell <ridercogswell@gmail.com>
2022-09-05 19:53:24 -0700

    signup request

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from app.models import User
-from app.db import User
+from app.models import User
 from app.db import get_db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
+
+@bp.route('/users', methods=['POST'])
+def signup():
+  data = request.get_json()
+  print(data)
+
+  return ''
