a449eda9bd529bae06df3ff4f5506f25a11868b6
Ridercogswell <ridercogswell@gmail.com>
2022-09-05 17:09:55 -0700

    formatting templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .routes import home, dashboard # or app.routes, same thing
 from app.db import init_db
+from app.utils import filters
 
 def create_app(test_config=None):
   # setup config
@@ -9,6 +10,10 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
+  # filter data
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 
   @app.route('/hello')
   def hello():
