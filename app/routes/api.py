f85559231c1ce24f9fb2cdc0d5dfc5810217cedb
Ridercogswell <ridercogswell@gmail.com>
2022-09-05 20:01:47 -0700

    pass data to user instanc

--- app/routes/api.py
+++ app/routes/api.py
@@ -8,11 +8,15 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
   data = request.get_json()
+  db = get_db() # connect to database
   
   newUser = User(
-    username = data.username, # dictionaries in python cannot be accessed like objects, must use bracket notation
-    email = data.email,
-    password = data.password
+    username = data['username'], # dictionaries in python cannot be accessed like objects, must use bracket notation
+    email = data['email'],
+    password = data['password']
   )
+
+  db.add(newUser)
+  db.commit() # add and then commit newUser
 
   return ''
