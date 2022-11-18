7775665e3e4079900bb79215c6af185a444ead67
Ridercogswell <ridercogswell@gmail.com>
2022-11-17 16:13:13 -0800

    fix root dir

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .routes import home, dashboard, api # or app.routes, same thing
+from app.routes import home, dashboard, api # or app.routes, same thing
 from app.db import init_db
 from app.utils import filters
 
@@ -14,11 +14,7 @@ def create_app(test_config=None):
   app.jinja_env.filters['format_url'] = filters.format_url
   app.jinja_env.filters['format_date'] = filters.format_date
   app.jinja_env.filters['format_plural'] = filters.format_plural
-
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
-
+  
   # register routes 
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
